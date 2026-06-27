537b2832ff6b00e64520762793b0f222b0749d12
Jesse Beck <thewebbeckons@users.noreply.github.com>
2026-06-27 09:48:13 -0400

    prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
 	nitro: {
 		preset: "cloudflare_module",
 	},
+	routeRules: {
+		"/": { prerender: true },
+	},
 	content: {
 		experimental: {
 			sqliteConnector: "native",
